5c36632d37ec9f33d1a95f0d3780b811f07e9684
Miguel Ramos <miguelrb@microsoft.com>
2021-08-24 16:36:13 -0700

    Add project files.

---
+++ WinUI3TemplateCpp/WinUI3TemplateCpp/SampleConfiguration.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "pch.h"
+
+namespace winrt::WinUI3TemplateCpp
+{
+    static class Settings
+    {
+    public:
+        hstring FetureName{ L"Windows App SDK Template C# Sample" };
+        hstring CurrentTheme{ L"" };
+    };
+}
---
+++ WinUI3TemplateCpp/WinUI3TemplateCpp/Scenario2_ShortName.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Scenario2_ShortName.g.h"
+
+namespace winrt::WinUI3TemplateCpp::implementation
+{
+    struct Scenario2_ShortName : Scenario2_ShortNameT<Scenario2_ShortName>
+    {
+        Scenario2_ShortName();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::WinUI3TemplateCpp::factory_implementation
+{
+    struct Scenario2_ShortName : Scenario2_ShortNameT<Scenario2_ShortName, implementation::Scenario2_ShortName>
+    {
+    };
+}
